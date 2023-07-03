04a4aa7dd8957eda7d6b2db4fa5f9f8060697d31
GabrielGallegosG <gallegosguzmang@gmail.com>
2023-07-03 13:44:52 -0700

    Reorganized folder structure and implemented load and error states

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { TodoCounter } from "./Components/TodoCounter/TodoCounter";
-import { TodoSearch } from "./Components/TodoSearch/TodoSearch";
-import { TodoList } from "./Components/TodoList/TodoList";
-import { TodoItem } from "./Components/TodoItem/TodoItem";
-import { CreateTodoButton } from "./Components/CreateTodoButton/CreateTodoButton";
+import { TodoCounter } from "../TodoCounter/TodoCounter";
+import { TodoSearch } from "../TodoSearch/TodoSearch";
+import { TodoList } from "../TodoList/TodoList";
+import { TodoItem } from "../TodoItem/TodoItem";
+import { CreateTodoButton } from "../CreateTodoButton/CreateTodoButton";
 import "./App.css";
+import { useLocalStorage } from "./useLocalStorage";
 
 // const defaultTodos = [
 //   { text: "Tomar el curso de React.js", completed: false },
@@ -13,30 +14,8 @@ import "./App.css";
 //   { text: "Crear un perfil de LinkedIn", completed: false },
 // ];
 
-function useLocalStorage(itemName, initialValue) {
-  const localStorageItem = localStorage.getItem(itemName);
-
-  let parsedItem;
-
-  if (localStorageItem) {
-    parsedItem = JSON.parse(localStorageItem);
-  } else {
-    localStorage.setItem(itemName, JSON.stringify(initialValue));
-    parsedItem = initialValue;
-  }
-
-  const [item, setItem] = useState(parsedItem);
-
-  const saveItem = (newItem) => {
-    localStorage.setItem(itemName, JSON.stringify(newItem));
-    setItem(newItem);
-  };
-
-  return [item, saveItem];
-}
-
 function App() {
-  const [tasks, saveTasks] = useLocalStorage("TODOS_V1", []);
+  const { item: tasks, saveItem: saveTasks, loading, error } = useLocalStorage("TODOS_V1", []);
   const [searchValue, setSearchValue] = useState("");
 
   const completedTasks = tasks.filter((tasks) => !!tasks.completed).length;
@@ -78,6 +57,11 @@ function App() {
         <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
 
         <TodoList>
+          {loading && <p>Cargando...</p>}
+          {error && <p>¡Error!</p>}
+          {!loading && searchedTasks.length === 0 && (
+            <p>No hay tareas para mostrar</p>
+          )}
           {searchedTasks.map((todo) => (
             <TodoItem
               key={todo.text}
